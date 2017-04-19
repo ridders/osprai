868629b331c290fae7490a2120a3fb0dfebd5a09
Simon Ridley <ridders7855@gmail.com>
2017-04-19 17:48:44 +0100

    Functions Updated
    Functions updated to accommodate changes in dir_parser.py

--- osprai.py
+++ osprai.py
@@ -7,10 +7,10 @@ from gi.repository import Gtk as gtk
 from gi.repository import Gdk as gdk
 from gi.repository.GdkPixbuf import Pixbuf
 from gi.repository import GdkPixbuf, Gdk
-from modules import folders
+from modules import dir_parser
 
 class image:
-
+	
 	def on_window1_destroy(self, object, data=None):
 		gtk.main_quit()
 		
@@ -18,43 +18,63 @@ class image:
 		self.filechooserdialog.hide()	
 	
 	def filechooserdialog_open_button_clicked(self, widget, data=None):
-		selection = self.filechooserdialog.get_current_folder()
+		self.selection = self.filechooserdialog.get_current_folder()
+		dir_parser.create_case(self.selection)
+		
+		total_count, uniques = dir_parser.create_index_and_thumbs(self.selection)
+		self.total_count.set_text(str(total_count))
+		self.total_unique.set_text(str(uniques))
 		self.filechooserdialog.hide()
 		
 		self.thumb_view.set_model(self.model)
 		self.thumb_view.set_pixbuf_column(0)
 		self.thumb_view.set_columns(-1)
 		
-		thumbs = folders.load_images(selection)
-		for thumbnail in thumbs:
+		self.desired_width = self.adjustment.get_value()
+		self.desired_height = self.adjustment.get_value()
 		
-			pixbuf = Pixbuf.new_from_file("temp/{0}".format(thumbnail))
-			pixbuf = pixbuf.scale_simple(self.desired_width, self.desired_height, GdkPixbuf.InterpType.HYPER)
-			self.model.append([pixbuf])
-
+		self.load_thumbs(self.selection,self.desired_width,self.desired_height)
+		
+	def load_thumbs(self, selection, width, height):
+		index = (os.path.join(selection, "case.osp", "index.csv"))
+		with open(index, "r") as fo:
+			for line in fo:
+				attribute = (line.split(","))
+				thumbs_path = attribute[2]
+				pixbuf = Pixbuf.new_from_file(thumbs_path)
+				pixbuf = pixbuf.scale_simple(self.desired_width, self.desired_height, GdkPixbuf.InterpType.HYPER)
+				self.model.append([pixbuf])
+		
 	def on_file_open_activate(self, menuitem, data=None):
 		self.filechooserdialog.run()
 		
 	def on_file_new_activate(self, menuitem, data=None):
 		self.filechooserdialog.run()
 	
-	def on_adjustment1_changed(self, widget, *argvs):
+	def on_adjustment1_changed(self, widget,*argvs):
 		self.model.clear()
 		self.desired_width = self.adjustment.get_value()
 		self.desired_height = self.adjustment.get_value()
-		self.filechooserdialog_open_button_clicked(self.desired_width,self.desired_height)
-	
+		self.load_thumbs(self.selection,self.desired_width,self.desired_height)
+
+				
 	def iconview_button_press_event(self, iconview, event):
 		if event.button == 3:
 			print("item right clicked")
 			self.popup.popup(None, None, None, None, event.button, event.time)
-			
+
 	def iconview_item_activated(self, widget, data=None):
 		print("item double clicked")
 	
 	def iconview_selection_changed(self, widget, data=None):
 		print("item selected")
-		#self.on_item_activated(widget, widget.get_selected_items()[0])
+		
+		
+	def show_result_activate(self, menuitem, data=None):
+		#print (self.thumb_view.get_selected_items()[0])
+		selected_path = (self.thumb_view.get_selected_items())
+		for f in selected_path:
+			print(f)
 		
 	def __init__(self):
 		self.gladefile = "gui.glade"
@@ -68,10 +88,9 @@ class image:
 		self.adjustment = self.builder.get_object('adjustment1')
 		self.filechooserdialog = self.builder.get_object("filechooserdialog1")
 		self.popup = self.builder.get_object("popup")
-				
-		self.desired_width = self.adjustment.get_value()
-		self.desired_height = self.adjustment.get_value()
-		
+		self.total_count = self.builder.get_object("label36")
+		self.total_unique = self.builder.get_object("label35")
+		self.selection = ""	
 		self.window.maximize()
 		self.window.show()
 		self.window.show_all()
