274d4089d9f4184611dcb56a9159d0c35279d180
Simon Ridley <ridders7855@gmail.com>
2017-04-20 17:22:17 +0100

    Add files via upload

--- modules/dir_parser.py
+++ modules/dir_parser.py
@@ -3,41 +3,51 @@ import os
 import sys
 import hashlib
 import re
-#from multiprocessing import Pool
 from datetime import datetime
 	
 def create_case(selection):	
 	for dirpath,_,filenames in os.walk(selection):
+		print("completed walking selection")
 		case_dir = (os.path.join(selection, "case.osp"))
 		thumbs_dir = (os.path.join(case_dir, "thumbs"))
 		if os.path.isdir(thumbs_dir) == False:
 			os.makedirs("{0}".format(thumbs_dir))
+			print("created thumbs directory")
 
 def create_index_and_thumbs(selection):
 	total_count = 0 #Quantity of media including ducplicates. This is being used as the unqiue ID for IconView
 	hashes = []
+	file_paths = []
 	case_dir = (os.path.join(selection, "case.osp"))
 	thumbs_dir = (os.path.join(case_dir, "thumbs"))
-	index = (os.path.join(selection, "case.osp", "index.csv"))	
+	index = (os.path.join(selection, "case.osp", "index.csv"))
+	
+	print("populating file path list")
+	for root,dirpath,filenames in os.walk(selection):
+		if "case.osp" in dirpath:	
+			dirpath.remove('case.osp')
+		for file in filenames:
+			file_path = (os.path.join(os.path.abspath(root),file))
+			file_paths.append(file_path)
+	print("file path list generated")
+	
+	print("commencing the population of index.csv")
 	with open(index, "a") as fo:
-		for root,dirpath,filenames in os.walk(selection):
-			dirpath.remove('case.osp') # Excludes the case folder from os.walk traversal
-
-		for f in filenames:
-			file_path = (os.path.join(selection, f))
-			md5 = hashlib.md5(open(file_path, 'rb').read()).hexdigest()
+		for item in file_paths:
+			md5 = hashlib.md5(open(item, 'rb').read()).hexdigest()
 			if md5 not in hashes:
 				hashes.append(md5)
-				
+				# create thumbs
 				size = 300, 300
-				im = cv2.imread(file_path)
+				im = cv2.imread(item)
 				resized_image = cv2.resize(im, size)
 				thumb_name = ("thumbnail_{0}.jpg".format(total_count))
 				thumbs_path = (os.path.join(thumbs_dir, thumb_name))
 				cv2.imwrite(thumbs_path, resized_image)
 					
-			fo.write("{0},{1},{2},{3}{4}".format(total_count,md5,thumbs_path,file_path,"\n"))
+			fo.write("{0},{1},{2},{3}{4}".format(total_count,md5,thumbs_path,item,"\n"))
 			total_count+=1
+		print("completed populating index.csv")
 				
 	uniques = (len(hashes))
 	return(total_count, uniques)
