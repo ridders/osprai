b91083d393e6b3a9fb237928a4be47180ccfb5ed
Simon Ridley <ridders7855@gmail.com>
2017-05-27 18:20:14 +0100

    Code streamlined

--- modules/dir_parser.py
+++ modules/dir_parser.py
@@ -1,55 +1,57 @@
 import cv2
 import os
 import sys
+import xxhash
 import hashlib
 import re
-from datetime import datetime
+from time import gmtime, strftime
 from os.path import splitext
-	
-def create_case(selection):	
+
+def create_case(selection):	# Althought not vital now, keep this for later when saving changes to file
 	for dirpath,_,filenames in os.walk(selection):
-		print("completed walking selection")
 		case_dir = (os.path.join(selection, "case.osp"))
 		thumbs_dir = (os.path.join(case_dir, "thumbs"))
 		if os.path.isdir(thumbs_dir) == False:
 			os.makedirs("{0}".format(thumbs_dir))
-			print("created thumbs directory")
 
 def create_index_and_thumbs(selection):
 	total_count = 0 #Quantity of media including ducplicates. This is being used as the unqiue ID for IconView
+	category = 0
 	hashes = []
-	file_paths = []
+	extensions = ('.jpeg', '.jpg', '.png', '.gif') # Prevents CV2 crashing on non image media files
 	case_dir = (os.path.join(selection, "case.osp"))
 	thumbs_dir = (os.path.join(case_dir, "thumbs"))
-	index = (os.path.join(selection, "case.osp", "index.csv"))
-	
-	print("populating file path list")
+	#index = (os.path.join(selection, "case.osp", "index.csv"))
+	index_file = [] #Replacement to above
+
+	print("commencing the population of virtual index")
+	print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 	for root,dirpath,filenames in os.walk(selection):
-		if "case.osp" in dirpath:	
+		if "case.osp" in dirpath:
 			dirpath.remove('case.osp')
-		for file in filenames:
-			file_path = (os.path.join(os.path.abspath(root),file))
-			file_paths.append(file_path)
-	print("file path list generated")
-	
-	print("commencing the population of index.csv")
-	with open(index, "a") as fo:
-		for item in file_paths:
-			md5 = hashlib.md5(open(item, 'rb').read()).hexdigest()
-			if md5 not in hashes:
-				hashes.append(md5)
-				# create thumbs
-				size = 300, 300
-				im = cv2.imread(item)
-				resized_image = cv2.resize(im, size)
-				filename,extension = splitext(item)
+		for item in filenames:
+			filename,extension = splitext(item)
+			if extension in extensions:
+				total_count+=1
+				file_path = (os.path.join(os.path.abspath(root),item))
 				thumb_name = ("thumbnail_{0}{1}".format(total_count,extension))
 				thumbs_path = (os.path.join(thumbs_dir, thumb_name))
-				cv2.imwrite(thumbs_path, resized_image)
-					
-			fo.write("{0},{1},{2},{3}{4}".format(total_count,md5,thumbs_path,item,"\n"))
-			total_count+=1
-		print("completed populating index.csv")
-				
+				xx = xxhash.xxh64(open(file_path, 'rb').read()).hexdigest() # !!! Stanity Check !!! does this work?
+				if xx not in hashes:
+					hashes.append(xx)
+
+			index_file.append("{0},{1},{2},{3}".format(xx,thumbs_path,file_path,category))
+			
+
+	print("completed populating virtual index")
+	print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 	uniques = (len(hashes))
-	return(total_count, uniques)
+	return(total_count, uniques, index_file)
+
+def thumbs_generator(file_loc, thumb_loc):
+	print(file_loc)
+	im = cv2.imread(file_loc)
+	r = 100.0 / im.shape[1]
+	dim = (100, int(im.shape[0] * r))
+	resized_image = cv2.resize(im, dim, interpolation = cv2.INTER_AREA)
+	cv2.imwrite(thumb_loc, resized_image)
