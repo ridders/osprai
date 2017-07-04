0d4fe880473c8004a95d162c0dc1dc0068d8dd5b
Simon Ridley <ridders7855@gmail.com>
2017-07-04 21:54:08 +0100

    Latest changes

--- modules/dir_parser.py
+++ modules/dir_parser.py
@@ -1,3 +1,4 @@
+#!python2
 import cv2
 import os
 import sys
@@ -6,13 +7,30 @@ import hashlib
 import re
 from time import gmtime, strftime
 from os.path import splitext
+try:
+    from os import scandir, walk
+except ImportError:
+    from scandir import scandir, walk
 
 def create_case(selection):	# Althought not vital now, keep this for later when saving changes to file
-	for dirpath,_,filenames in os.walk(selection):
+    status = 0
+    extensions = ('.jpeg', '.jpg', '.png', '.gif')
+    print("commencing initial scan")
+    print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+    for dirpath,_,filenames in os.walk(selection):
 		case_dir = (os.path.join(selection, "case.osp"))
 		thumbs_dir = (os.path.join(case_dir, "thumbs"))
 		if os.path.isdir(thumbs_dir) == False:
 			os.makedirs("{0}".format(thumbs_dir))
+		for item in filenames:
+			#print(item)
+			filename,extension = splitext(item)
+			if extension in extensions:
+				status+=1
+    return status
+    print("initial scan complete")
+    print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+			#add a return of the files in here somewhere
 
 def create_index_and_thumbs(selection):
 	total_count = 0 #Quantity of media including ducplicates. This is being used as the unqiue ID for IconView
@@ -34,19 +52,34 @@ def create_index_and_thumbs(selection):
 			if extension in extensions:
 				total_count+=1
 				file_path = (os.path.join(os.path.abspath(root),item))
+				#print(file_path)
 				thumb_name = ("thumbnail_{0}{1}".format(total_count,extension))
 				thumbs_path = (os.path.join(thumbs_dir, thumb_name))
-				xx = xxhash.xxh64(open(file_path, 'rb').read()).hexdigest() # !!! Stanity Check !!! does this work?
-				if xx not in hashes:
-					hashes.append(xx)
+				xx = hash_file(file_path)
+				#print(xx)
+				#if xx not in hashes:
+					#hashes.append(xx)
+			
+			
 
 			index_file.append("{0},{1},{2},{3}".format(xx,thumbs_path,file_path,category))
 			
 
 	print("completed populating virtual index")
+	print(total_count)
 	print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-	uniques = (len(hashes))
-	return(total_count, uniques, index_file)
+	#uniques = (len(hashes))
+	return(index_file)
+	
+def hash_file(file_path, block_size=256 * 128):
+    hasher = xxhash.xxh64()
+    with open(file_path, 'rb') as file:
+        while True:
+            buf = file.read(block_size)
+            if not buf:
+                break
+            hasher.update(buf)
+        return hasher.hexdigest()
 
 def thumbs_generator(file_loc, thumb_loc):
 	print(file_loc)
